0c0813fb02e7b964765a0c563b8ae02504eff315
Luis Rocha <lerocha@gmail.com>
2017-03-31 00:19:13 -0700

    Hook up the Tumblr API

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class PhotosViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let apiKey = "Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV"
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=\(apiKey)")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(with: request,completionHandler: { (dataOrNil, response, error) in
+            if let data = dataOrNil {
+                if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as? NSDictionary {
+                    
+                    let response = responseDictionary["response"] as? NSDictionary
+                    let posts = response?["posts"] as? [NSDictionary]
+                    NSLog("response: \(posts)")
+                }
+            }
+        });
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
